0d9aed13cd4f71d8141292284236cf72be7245c5
David Tschumperlé <David.Tschumperle@gmail.com>
2019-05-11 16:20:23 +0200

    .

--- src/gmic.cpp
+++ src/gmic.cpp
@@ -8757,6 +8757,14 @@ gmic& gmic::_run(const CImgList<char>& commands_line, unsigned int& position,
           else { pattern = 0; is_cond = false; }
           boundary = pattern%3;
           CImg<char>::string(argument + (is_cond?2:0)).get_split(CImg<char>::vector(','),0,false).move_to(g_list_c);
+
+          // Detect possible empty names in argument list.
+          bool is_empty_name = false;
+          unsigned int sl = 0;
+          for (; argument[sl]; ++sl) if (argument[sl]==',' && argument[sl+1]==',') { is_empty_name = true; break; }
+          if (!is_empty_name && (*argument==',' || argument[sl - 1]==',')) is_empty_name = true;
+          if (is_empty_name) CImg<char>(1,1,1,1,0).move_to(g_list_c);
+
           print(images,0,"Get %s%s with name%s '%s' for image%s (case-%s).",
                 boundary==0?"all image ind":boundary==1?"lowest image ind":"highest image ind",
                 boundary==0 || g_list_c.size()>1?"ices":"ex",
