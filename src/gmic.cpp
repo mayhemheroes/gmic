f71a3eee8e84ff038a1e83d8d0837e63b78356fd
David Tschumperlé <David.Tschumperle@gmail.com>
2019-12-18 09:57:00 +0100

    .

--- src/gmic.cpp
+++ src/gmic.cpp
@@ -3056,7 +3056,7 @@ const char *gmic::set_variable(const char *const name, const CImg<unsigned char>
 //------------------------------------------
 gmic& gmic::add_commands(const char *const data_commands, const char *const commands_file,
                          unsigned int *count_new, unsigned int *count_replaced,
-                         bool *is_cli_start_command) {
+                         bool *is_init_command) {
   if (!data_commands || !*data_commands) return *this;
   cimg::mutex(23);
   CImg<char> s_body(256*1024), s_line(256*1024), s_name(256), debug_info(32);
@@ -3099,13 +3099,16 @@ gmic& gmic::add_commands(const char *const data_commands, const char *const comm
     if (!*lines) continue; // Empty line found
     *s_name = *s_body = 0;
 
-    if (!is_last_slash && std::strchr(lines,':') && // Check for a command definition
-        cimg_sscanf(lines,"%255[a-zA-Z0-9_] %c %262143[^\n]",s_name.data(),&sep,s_body.data())>=2 &&
-        (*lines<'0' || *lines>'9') && sep==':') {
-
-      if (is_cli_start_command) *is_cli_start_command|=!std::strcmp(s_name,"cli_start");
+    if (hash<0 || // Check for new command definition (or implicit '__init__')
+        (!is_last_slash && std::strchr(lines,':') &&
+         cimg_sscanf(lines,"%255[a-zA-Z0-9_] %c %262143[^\n]",s_name.data(),&sep,s_body.data())>=2 &&
+         (*lines<'0' || *lines>'9') && sep==':')) {
+      CImg<char> body = CImg<char>::string(hash<0?lines:s_body);
+      if (hash<0) {
+        std::strcpy(s_name,"__init__");
+        if (is_init_command) *is_init_command = true;
+      }
       hash = (int)hashcode(s_name,false);
-      CImg<char> body = CImg<char>::string(s_body);
       if (commands_file) { // Insert debug info code in body
         if (commands_files.width()<2)
           l_debug_info = cimg_snprintf(debug_info.data() + 1,debug_info.width() - 2,"%x",line_number);
@@ -3128,7 +3131,6 @@ gmic& gmic::add_commands(const char *const data_commands, const char *const comm
       body.move_to(commands[hash][pos]);
 
     } else { // Continuation of a previous line
-      if (hash<0) error(true,"Command 'command': Syntax error in expression '%s'.",lines);
       if (!is_last_slash) commands[hash][pos].back() = ' ';
       else --(commands[hash][pos]._width);
       const CImg<char> body = CImg<char>(lines,(unsigned int)(linee - lines + 2));
@@ -3166,7 +3168,7 @@ gmic& gmic::add_commands(const char *const data_commands, const char *const comm
 //---------------------------
 gmic& gmic::add_commands(std::FILE *const file, const char *const filename,
                          unsigned int *count_new, unsigned int *count_replaced,
-                         bool *is_cli_start_command) {
+                         bool *is_init_command) {
   if (!file) return *this;
 
   // Try reading it first as a .cimg file.
@@ -3174,7 +3176,7 @@ gmic& gmic::add_commands(std::FILE *const file, const char *const filename,
     CImg<char> buffer;
     buffer.load_cimg(file).unroll('x');
     buffer.resize(buffer.width() + 1,1,1,1,0);
-    add_commands(buffer.data(),filename,count_new,count_replaced,is_cli_start_command);
+    add_commands(buffer.data(),filename,count_new,count_replaced,is_init_command);
   } catch (...) {
     std::rewind(file);
     std::fseek(file,0,SEEK_END);
@@ -3184,7 +3186,7 @@ gmic& gmic::add_commands(std::FILE *const file, const char *const filename,
       CImg<char> buffer((unsigned int)siz + 1);
       if (std::fread(buffer.data(),sizeof(char),siz,file)) {
         buffer[siz] = 0;
-        add_commands(buffer.data(),filename,count_new,count_replaced,is_cli_start_command);
+        add_commands(buffer.data(),filename,count_new,count_replaced,is_init_command);
       }
     }
   }
@@ -14428,7 +14430,7 @@ gmic& gmic::_run(const CImgList<char>& commands_line, unsigned int& position,
           unsigned int count_new = 0, count_replaced = 0;
           std::FILE *const gfile = cimg::fopen(filename,"rb");
 
-          bool is_cli_start_command = false;
+          bool is_init_command = false;
           bool is_add_error = false;
           status.move_to(o_status); // Save status because 'add_commands' can change it, with error()
           const int o_verbosity = verbosity;
@@ -14436,9 +14438,9 @@ gmic& gmic::_run(const CImgList<char>& commands_line, unsigned int& position,
           verbosity = 0;
           is_debug = false;
           try {
-            add_commands(gfile,add_debug_info?filename:0,&count_new,&count_replaced,&is_cli_start_command);
+            add_commands(gfile,add_debug_info?filename:0,&count_new,&count_replaced,&is_init_command);
           } catch (...) {
-            is_add_error = true; is_cli_start_command = false;
+            is_add_error = true; is_init_command = false;
           }
 
           is_debug = o_is_debug;
@@ -14471,7 +14473,7 @@ gmic& gmic::_run(const CImgList<char>& commands_line, unsigned int& position,
             std::fflush(cimg::output());
             cimg::mutex(29,0);
           }
-          if (callstack.size()==1 && is_cli_start_command &&
+          if (callstack.size()==1 && is_init_command &&
               !std::strcmp(set_variable("_host","",'.',0),"cli")) { // Execute 'cli_start' command
             const CImgList<char> ncommands_line = commands_line_to_CImgList("cli_start");
             unsigned int nposition = 0;
