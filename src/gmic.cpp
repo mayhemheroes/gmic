d2d89df10c5cf101c100f591cfe7b8cd68b7f1b7
David Tschumperlé <david.tschumperle@gmail.com>
2019-12-18 13:33:22 +0100

    .

--- src/gmic.cpp
+++ src/gmic.cpp
@@ -3099,15 +3099,11 @@ gmic& gmic::add_commands(const char *const data_commands, const char *const comm
     if (!*lines) continue; // Empty line found
     *s_name = *s_body = 0;
 
-    if ((is_runfile && hash<0) || // Check for a command definition (or implicit '__init__')
-        (!is_last_slash && std::strchr(lines,':') &&
+    if ((!is_last_slash && std::strchr(lines,':') && // Check for a command definition (or implicit '__init__')
          cimg_sscanf(lines,"%255[a-zA-Z0-9_] %c %262143[^\n]",s_name.data(),&sep,s_body.data())>=2 &&
-         (*lines<'0' || *lines>'9') && sep==':')) {
+         (*lines<'0' || *lines>'9') && sep==':') || hash<0) {
       CImg<char> body = CImg<char>::string(hash<0 && !*s_name?lines:s_body);
-      if (hash<0 && !*s_name) {
-        std::strcpy(s_name,"__init__");
-        if (is_init_command) *is_init_command = true;
-      }
+      if (hash<0 && !*s_name) { std::strcpy(s_name,"__init__"); if (is_init_command) *is_init_command = true; }
       hash = (int)hashcode(s_name,false);
 
       if (commands_file) { // Insert debug info code in body
@@ -3132,7 +3128,6 @@ gmic& gmic::add_commands(const char *const data_commands, const char *const comm
       body.move_to(commands[hash][pos]);
 
     } else { // Continuation of a previous line
-      if (hash<0) error(true,"Command 'command': Syntax error in expression '%s'.",lines);
       if (!is_last_slash) commands[hash][pos].back() = ' ';
       else --(commands[hash][pos]._width);
       const CImg<char> body = CImg<char>(lines,(unsigned int)(linee - lines + 2));
@@ -14433,16 +14428,17 @@ gmic& gmic::_run(const CImgList<char>& commands_line, unsigned int& position,
           unsigned int count_new = 0, count_replaced = 0;
           std::FILE *const gfile = cimg::fopen(filename,"rb");
 
-          bool is_add_error = false;
+          bool is_init_command = false, is_add_error = false;
           status.move_to(o_status); // Save status because 'add_commands' can change it, with error()
           const int o_verbosity = verbosity;
           const bool o_is_debug = is_debug;
           verbosity = 0;
           is_debug = false;
           try {
-            add_commands(gfile,add_debug_info?filename:0,&count_new,&count_replaced);
+            add_commands(gfile,add_debug_info?filename:0,&count_new,&count_replaced,
+                         is_runfile && callstack.size()==1?&is_init_command:0);
           } catch (...) {
-            is_add_error = true;
+            is_add_error = true; is_init_command = false;
           }
           is_debug = o_is_debug;
           verbosity = o_verbosity;
@@ -14474,7 +14470,17 @@ gmic& gmic::_run(const CImgList<char>& commands_line, unsigned int& position,
             std::fflush(cimg::output());
             cimg::mutex(29,0);
           }
+          if (is_init_command) { // Execute '__init__' command
+            const CImgList<char> ncommands_line = commands_line_to_CImgList("__init__");
+            unsigned int nposition = 0;
+            CImg<char>::string("").move_to(callstack); // Anonymous scope
+            const int o_verbosity = verbosity++;
+            _run(ncommands_line,nposition,images,images_names,parent_images,parent_images_names,variables_sizes,0,0,0);
+            verbosity = o_verbosity;
+            callstack.remove();
+          }
           continue;
+
         } else { // Other file types.
 
           // Check if a custom command handling requested file format exists.
