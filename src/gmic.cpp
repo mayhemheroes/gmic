18f1b9d73c0eb525e9abf0ae0d6d94f9e3663fd6
David Tschumperle <David.Tschumperle@unicaen.fr>
2022-10-25 18:32:49 +0200

    .

--- src/gmic.cpp
+++ src/gmic.cpp
@@ -7502,8 +7502,7 @@ gmic& gmic::_run(const CImgList<char>& commands_line, unsigned int& position,
               is_debug_info|=get_debug_info(commands_line[position].data(),next_debug_line,next_debug_filename);
             else {
               it+=*it=='-';
-              if (!std::strcmp("if",it)) ++nb_levels;
-              else if (!std::strcmp("fi",it)) { if (!--nb_levels) --position; }
+              gmic_if_diflr ++nb_levels; gmic_elif_diflr { --nb_levels; if (!nb_levels) --position; }
             }
           }
           continue;
@@ -13664,8 +13663,7 @@ gmic& gmic::_run(const CImgList<char>& commands_line, unsigned int& position,
                   is_debug_info|=get_debug_info(commands_line[position].data(),next_debug_line,next_debug_filename);
                 else {
                   it+=*it=='-';
-                  if (!std::strcmp("if",it)) ++nb_levels;
-                  else if (!std::strcmp("fi",it)) { --nb_levels; if (!nb_levels) --position; }
+                  gmic_if_diflr ++nb_levels; gmic_elif_diflr { --nb_levels; if (!nb_levels) --position; }
                   else if (nb_levels==1) {
                     if (!std::strcmp("else",it)) --nb_levels;
                     else if (!std::strcmp("elif",it)) { --nb_levels; check_elif = true; --position; }
