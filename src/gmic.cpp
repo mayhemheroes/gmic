32a16360e8aa2e0b12cbcafc646a3af261770586
David Tschumperle <David.Tschumperle@unicaen.fr>
2023-01-11 12:30:44 +0100

    .

--- src/gmic.cpp
+++ src/gmic.cpp
@@ -504,7 +504,7 @@ CImg<T>& gmic_draw_text(const float x, const float y,
     fx = sepx=='%' || sepx=='~'?0:x;
     fy = sepy=='%' || sepy=='~'?0:y;
     draw_text((int)cimg::round(fx),(int)cimg::round(fy),"%s",one,0,opacity,siz,text).resize(-100,-100,1,nb_cols);
-    cimg_forC(*this,c) get_shared_channel(c)*=col[c];
+    cimg_forC(*this,c) if (col[c]!=1) get_shared_channel(c)*=col[c];
     return *this;
   }
   if (sepx=='~' || sepy=='~') {
